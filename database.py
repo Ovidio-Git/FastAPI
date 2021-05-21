747580802001fa0588d433c3e929336be5c1ab48
Ovidio Andrade <oviidio.andrade@gmail.com>
2021-05-21 11:09:33 -0500

    adding database conf and update docker files

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+import datetime
 from peewee import *
 
 database_name='test'
@@ -9,3 +10,18 @@ database = MySQLDatabase(
     password='991121',
     host='localhost'
 )
+
+#table representation
+class Dogs(Model):
+    id =IntegerField(unique=True, null=False)
+    name = CharField(max_length=20, null=False)
+    picture = CharField(max_length=100)
+    is_adopted = BooleanField(default=True)
+    created_date = DateTimeField(default=datetime.datetime.now)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        database = database
+        tabel_name = 'Dogs'
--- dockerfile
+++ dockerfile
@@ -1,8 +1,12 @@
-#st
 FROM tiangolo/uvicorn-gunicorn-fastapi:python3.8
 
-RUN pip
+# Copy requirements to docker container
+COPY requirements.txt .
 
+# Install requirements on docker container
+RUN pip3 install -r requirements.txt
+
+# Expose port 3000 of container
 EXPOSE 3000
 
 COPY ./app /app
