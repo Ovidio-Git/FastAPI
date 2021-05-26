a33fe70cb0e636e33722c86ffcea4edf09adb149
Ovidio Andrade <oviidio.andrade@gmail.com>
2021-05-25 21:25:20 -0500

    sin comentarios.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from database import database as MySQL
 from database import Dogs
 from schemas import UserRequestModel
+from schemas import UserResponseModel
 
 app = FastAPI(title='Dogs', version='1.2')
 
@@ -21,19 +22,43 @@ async def finish():
         MySQL.close()
 
 
-
-
 @app.get("/api/dogs")
 async def index():
-    return "list"
+    response = []
+    for dog in Dogs.select():
+        response.append( UserResponseModel(
+        id = dog.id,
+        name = dog.name,
+        picture = dog.picture,
+        is_adopted=dog.is_adopted,
+        created_date=dog.created_date))
+    return response
 
 @app.get("/api/dogs/{name}")
-async def names(name: str):
-    return "names"
+async def get_names(name):
+    response = []
+    for dog in Dogs.select().where(Dogs.name == name):
+        response.append( UserResponseModel(
+        id = dog.id,
+        name = dog.name,
+        picture = dog.picture,
+        is_adopted=dog.is_adopted,
+        created_date=dog.created_date))
+    return response
+
 
 @app.get("/api/dogs/is_adopted")
-async def adopted():
-    return "is_adopted"
+async def dogs_adopted():
+    response = []
+    for dog in Dogs.select().where(Dogs.name == 1):
+        response.append( UserResponseModel(
+        id = dog.id,
+        name = dog.name,
+        picture = dog.picture,
+        is_adopted=dog.is_adopted,
+        created_date=dog.created_date))
+    return response
+
 
 @app.post("/api/dogs/{name}")
 async def create_dog(dog_request: UserRequestModel):
--- dockerfile
+++ dockerfile
@@ -1,7 +1,8 @@
 FROM tiangolo/uvicorn-gunicorn-fastapi:python3.8
-
+# directory for work
+WORKDIR /app
 # Copy requirements to docker container
-COPY requirements.txt .
+COPY requirements.txt requirements.txt
 
 # Install requirements on docker container
 RUN pip3 install -r requirements.txt
@@ -9,4 +10,5 @@ RUN pip3 install -r requirements.txt
 # Expose port 3000 of container
 EXPOSE 3000
 
-COPY ./app /app
+COPY . .
+CMD ["uvicorn", "app:app", "--host", "127.0.0.1", "--port", "3000"]
